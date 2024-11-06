babca5e0c8693fe75ec44804d28ebdb284eed072
gregschram <greg@gregschram.com>
2024-11-06 01:32:40 -0800

    Update categories.js

--- src/data/categories.js
+++ src/data/categories.js
@@ -2,6 +2,7 @@ export const categories = {
   GROCERY: {
     id: 'grocery',
     name: 'Grocery Store',
-    description: 'Common phrases and words you might hear at the grocery store'
+    description: 'Common phrases you might hear at the grocery store'
   }
+  // Add more categories here as needed
 };
