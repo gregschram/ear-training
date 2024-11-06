e2a0b9fe1758b8d1141ab8516a70898cc3e9164e
gregschram <greg@gregschram.com>
2024-11-06 00:55:48 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const EXERCISES = {
 
 const HomePage = ({ onSelectExercise }) => {
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
+    <div className="min-h-screen bg-red-500 p-8">  // This should make the background bright red
       <div className="max-w-2xl mx-auto">
         <h1 className="text-3xl font-bold mb-6">Hearing Training Exercises</h1>
         <div className="grid gap-6">
