f18f4ea1f561c4e3794cf04ee872cf74b2d350e7
gregschram <greg@gregschram.com>
2024-11-06 01:30:41 -0800

    Update AudioPlayer.js

--- src/components/Exercisegame/components/AudioPlayer/AudioPlayer.js
+++ src/components/Exercisegame/components/AudioPlayer/AudioPlayer.js
@@ -1,1 +1,35 @@
+import { PlayCircle, Rewind, FastForward } from 'lucide-react';
+import styles from './AudioPlayer.module.css';
 
+const AudioPlayer = ({ audioRef, playbackSpeed, onPlay, onSpeedToggle }) => {
+  return (
+    <div className={styles.container}>
+      <button 
+        onClick={onPlay}
+        className={styles.playButton}
+      >
+        <PlayCircle className={styles.icon} />
+        Play Sound {playbackSpeed !== 1 ? '(Slow)' : ''}
+      </button>
+
+      <button
+        onClick={onSpeedToggle}
+        className={styles.speedButton}
+      >
+        {playbackSpeed === 1 ? (
+          <>
+            <Rewind className={styles.icon} />
+            Switch to Slow Speed
+          </>
+        ) : (
+          <>
+            <FastForward className={styles.icon} />
+            Switch to Normal Speed
+          </>
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default AudioPlayer;
