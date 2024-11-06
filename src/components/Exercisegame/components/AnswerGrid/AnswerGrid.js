4daee5921acfa7cbb73fa631ba4e48b4016698bb
gregschram <greg@gregschram.com>
2024-11-06 01:31:19 -0800

    Update AnswerGrid.js

--- src/components/Exercisegame/components/AnswerGrid/AnswerGrid.js
+++ src/components/Exercisegame/components/AnswerGrid/AnswerGrid.js
@@ -1,1 +1,30 @@
+import styles from './AnswerGrid.module.css';
 
+const AnswerGrid = ({ 
+  options, 
+  selectedAnswer, 
+  correctAnswer, 
+  gameState, 
+  onOptionSelect 
+}) => {
+  return (
+    <div className={styles.grid}>
+      {options.map((option) => (
+        <button
+          key={option}
+          onClick={() => onOptionSelect(option)}
+          disabled={gameState === 'showing_result'}
+          className={`
+            ${styles.option}
+            ${gameState === 'showing_result' && option === correctAnswer ? styles.correct : ''}
+            ${gameState === 'showing_result' && option === selectedAnswer && option !== correctAnswer ? styles.incorrect : ''}
+          `}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default AnswerGrid;
