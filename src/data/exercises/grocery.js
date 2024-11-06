333ef364946908e686eeabadde29819e89fb3e29
gregschram <greg@gregschram.com>
2024-11-05 16:11:26 -0800

    Create grocery.js

---
+++ src/data/exercises/grocery.js
@@ -0,0 +1,18 @@
+export const groceryExercises = {
+  sentences: {
+    items: [
+      {
+        id: 1,
+        audioPath: '/audio/grocery/sentence1.mp3',
+        sentence: 'Do we need milk?',
+        options: [
+          'Do we need milk?',
+          'Do we need meat?',
+          'Do we have milk?',
+          'Can I get milk?'
+        ]
+      }
+      // You'll add more items here later
+    ]
+  }
+};
