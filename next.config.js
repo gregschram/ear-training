6c3b5f5542b6f90093e5c7f13f56f2fbf0e6321f
gregschram <greg@gregschram.com>
2024-11-06 01:26:16 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add this to ensure static files are handled correctly
-  async headers() {
-    return [
-      {
-        source: '/audio/:path*',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'audio/mpeg',
-          },
-        ],
-      },
-    ];
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
